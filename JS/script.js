1ee42cf7f3ac25b536feb04a1b20c8b2d089069b
Risul-Islam-Tushar <risultushar@gmail.com>
2023-10-08 12:06:37 +0600

    grid added

--- JS/script.js
+++ JS/script.js
@@ -22,3 +22,73 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+// Flip Card 1
+document.getElementById("card").addEventListener("mouseenter", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+
+document.getElementById("card").addEventListener("mouseleave", function () {
+  this.style.transform = "rotateY(0deg)";
+});
+// Flip Card 2
+document.getElementById("card2").addEventListener("mouseenter", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card2").addEventListener("mouseleave", function () {
+  this.style.transform = "rotateY(0deg)";
+});
+// Flip Card 3
+document.getElementById("card3").addEventListener("mouseenter", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+
+document.getElementById("card3").addEventListener("mouseleave", function () {
+  this.style.transform = "rotateY(0deg)";
+});
+// Flip Card 4
+document.getElementById("card4").addEventListener("mouseenter", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+
+document.getElementById("card4").addEventListener("mouseleave", function () {
+  this.style.transform = "rotateY(0deg)";
+});
+// Flip Card 5
+document.getElementById("card5").addEventListener("mouseenter", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+
+document.getElementById("card5").addEventListener("mouseleave", function () {
+  this.style.transform = "rotateY(0deg)";
+});
+// Flip Card 6
+document.getElementById("card6").addEventListener("mouseenter", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+
+document.getElementById("card6").addEventListener("mouseleave", function () {
+  this.style.transform = "rotateY(0deg)";
+});
+// Flip Card 7
+document.getElementById("card7").addEventListener("mouseenter", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+
+document.getElementById("card7").addEventListener("mouseleave", function () {
+  this.style.transform = "rotateY(0deg)";
+});
+// Flip Card 8
+document.getElementById("card8").addEventListener("mouseenter", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+
+document.getElementById("card8").addEventListener("mouseleave", function () {
+  this.style.transform = "rotateY(0deg)";
+});
+
+$(function () {
+  $("#datepicker1").datepicker();
+});
+$(function () {
+  $("#datepicker2").datepicker();
+});
